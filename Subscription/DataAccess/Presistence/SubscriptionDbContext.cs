6691070e0d3f8e30bae47b3cf02250a93a0a85b4
Pavlo Voloshyn <pavlo_voloshyn@epam.com>
2022-02-15 18:31:23 +0200

    Create database for subscription

---
+++ Subscription/DataAccess/Presistence/SubscriptionDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Presistence.Mapping;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Presistence
+{
+    public class SubscriptionDbContext : DbContext
+    {
+        public DbSet<Subscription> Subscriptions { get; set; }
+
+        public SubscriptionDbContext(DbContextOptions<SubscriptionDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SubscriptionConfiguration.Configure(modelBuilder);
+        }
+    }
+}
---
+++ Subscription/Domain/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string? FoundationId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateSubscribed { get; set; }
+        public bool IsCanceled { get; set; }
+    }
+}

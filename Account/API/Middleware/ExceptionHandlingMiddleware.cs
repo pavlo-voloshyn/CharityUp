63d5c6d44694d11c75e14454669759ec8989b583
Pavlo Voloshyn <pavlo_voloshyn@epam.com>
2022-02-23 13:13:03 +0200

    Refactor

--- Account/API/Middleware/ExceptionHandlingMiddleware.cs
+++ Account/API/Middleware/ExceptionHandlingMiddleware.cs
@@ -2,48 +2,52 @@
 using System.Net;
 using System.Text.Json;
 
-namespace API.Middleware
-{
-    public class ExceptionHandlerMiddleware
-    {
-        private readonly RequestDelegate _next;
+namespace API.Middleware;
 
-        public ExceptionHandlerMiddleware(RequestDelegate next)
+internal class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
         {
-            _next = next;
+            await _next(httpContext);
         }
-        public async Task InvokeAsync(HttpContext httpContext)
+        catch (Exception ex)
         {
-            try
-            {
-                await _next(httpContext);
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(httpContext, ex);
-            }
+            await HandleExceptionAsync(httpContext, ex);
         }
-        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        
+        if (exception is ArgumentException || exception is FormatException)
         {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            if (exception is ArgumentException || exception is FormatException)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            }
-            else if (exception is InvalidOperationException
-                || exception is DbUpdateException
-                || exception is DbUpdateConcurrencyException
-                || exception is OperationCanceledException)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
-            }
-            var result = JsonSerializer.Serialize(new 
-            {
-                message = exception.Message,
-                innerExceptionMessage = exception.InnerException?.Message
-            });
-            await context.Response.WriteAsync(result);
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
         }
+        else if (exception is InvalidOperationException
+            || exception is DbUpdateException
+            || exception is DbUpdateConcurrencyException
+            || exception is OperationCanceledException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+        }
+        
+        var result = JsonSerializer.Serialize(new
+        {
+            message = exception.Message,
+            innerExceptionMessage = exception.InnerException?.Message
+        });
+        
+        await context.Response.WriteAsync(result);
     }
 }

78dd44dc5a8eeefa3db3e5184eedc798e9e6fee9
Pavlo Voloshyn <pavlo_voloshyn@epam.com>
2022-03-05 23:03:08 +0800

    Use config extensions

---
+++ Web.ApiGateway/Configurations/DependencyInjectionConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Web.ApiGateway.Services;
+
+namespace Web.ApiGateway.Configurations;
+
+public static class DependencyInjectionConfiguration
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<AccountHttpClientService>();
+        services.AddSingleton<FoundationHttpClientService>();
+        services.AddSingleton<PaymentHttpClientService>();
+        services.AddSingleton<SubscriptionHttpClientService>();
+    }
+
+    public static void AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient("AccountService", config =>
+        {
+            config.BaseAddress = new Uri(configuration["Services:Account"]);
+        });
+        services.AddHttpClient("FoundationService", config =>
+        {
+            config.BaseAddress = new Uri(configuration["Services:Foundation"]);
+        });
+        services.AddHttpClient("PaymentService", config =>
+        {
+            config.BaseAddress = new Uri(configuration["Services:Payment"]);
+        });
+        services.AddHttpClient("SubscriptionService", config =>
+        {
+            config.BaseAddress = new Uri(configuration["Services:Subscription"]);
+        });
+    }
+}
--- Web.ApiGateway/Program.cs
+++ Web.ApiGateway/Program.cs
@@ -1,4 +1,4 @@
-using Web.ApiGateway.Services;
+using Web.ApiGateway.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,26 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient("AccountService", config =>
-{
-    config.BaseAddress = new Uri(builder.Configuration["Services:Account"]);
-});
-builder.Services.AddHttpClient("FoundationService", config =>
-{
-    config.BaseAddress = new Uri(builder.Configuration["Services:Foundation"]);
-});
-builder.Services.AddHttpClient("PaymentService", config =>
-{
-    config.BaseAddress = new Uri(builder.Configuration["Services:Payment"]);
-});
-builder.Services.AddHttpClient("SubscriptionService", config =>
-{
-    config.BaseAddress = new Uri(builder.Configuration["Services:Subscription"]);
-});
-builder.Services.AddSingleton<AccountHttpClientService>();
-builder.Services.AddSingleton<FoundationHttpClientService>();
-builder.Services.AddSingleton<PaymentHttpClientService>();
-builder.Services.AddSingleton<SubscriptionHttpClientService>();
+builder.Services.AddHttpClients(builder.Configuration);
+builder.Services.AddServices();
 
 var app = builder.Build();
 
